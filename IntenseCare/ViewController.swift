d10cdeee2fef3ffe13aa0a4562c985f41fdf1da2
Felipe Cantagalli <fcantaga@purdue.edu>
2015-03-14 12:23:44 -0300

    Initial Commit

---
+++ IntenseCare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntenseCare
+//
+//  Created by Felipe on 3/14/15.
+//  Copyright (c) 2015 felanca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
