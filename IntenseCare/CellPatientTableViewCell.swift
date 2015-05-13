6c5c0efaa8f00e75ee96d1271846a53ecb443ee8
Felipe Cantagalli <fcantaga@purdue.edu>
2015-05-12 23:08:30 -0300

    Implementing alertViews to update status and update bed

--- IntenseCare/CellPatientTableViewCell.swift
+++ IntenseCare/CellPatientTableViewCell.swift
@@ -22,7 +22,7 @@ class CellPatientTableViewCell: UITableViewCell {
     @IBAction func ExtraButton(sender: AnyObject) {
         
         let alert = UIAlertController(title: nil, message: nil , preferredStyle: UIAlertControllerStyle.ActionSheet)
-        
+        // BUTTON TO DO THE CONSULTATION
         let consultation = UIAlertAction(title: "Do consultation", style: UIAlertActionStyle.Default) { (alert) -> Void in
             println("consultation")
             var storyboard = UIStoryboard(name: "Consultation", bundle: nil)
@@ -31,19 +31,46 @@ class CellPatientTableViewCell: UITableViewCell {
             tabViewController.barColor = self.contentView.backgroundColor
             self.viewController?.showViewController(tabViewController, sender: self.viewController)
         }
-        let history = UIAlertAction(title: "Change bed", style: UIAlertActionStyle.Default) { (alert) -> Void in
-            println("history")
+        // BUTTON TO CHANGE PATIENT BED. It creates a alertView with a textfield for input, a save and canecel button.
+        let changeBed = UIAlertAction(title: "Change bed", style: UIAlertActionStyle.Default) { (alert) -> Void in
+            println("changing bed")
+            
+            var alertview = UIAlertController(title: "Chaging bed number for patient "+self.patientName.text!, message: "Insert the new bed number", preferredStyle: UIAlertControllerStyle.Alert)
+            var saveButton = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: { (sender:UIAlertAction!) -> Void in
+                // code to save
+            })
+            var cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            alertview.addTextFieldWithConfigurationHandler({ (textfield:UITextField!) -> Void in
+                textfield.placeholder = "Insert new bed number here"
+            })
+            alertview.addAction(saveButton)
+            alertview.addAction(cancelButton)
+            self.viewController?.presentViewController(alertview, animated: false, completion: nil)
         }
-        let blabla = UIAlertAction(title: "Change status", style: UIAlertActionStyle.Default) { (alert) -> Void in
-            println("Bla Bla")
+        // BUTTON TO CHANGE PATIENT STATUS. It creates a alertView with a textfield for input, a save and canecel button.
+        let changeStatus = UIAlertAction(title: "Change status", style: UIAlertActionStyle.Default) { (alert) -> Void in
+            println("changing status")
+            
+            var alertview = UIAlertController(title: "Chaging status for patient "+self.patientName.text!, message: "Insert the new patient status", preferredStyle: UIAlertControllerStyle.Alert)
+            var saveButton = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: { (sender:UIAlertAction!) -> Void in
+                // code to save
+            })
+            var cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            alertview.addTextFieldWithConfigurationHandler({ (textfield:UITextField!) -> Void in
+                textfield.placeholder = "Insert new status number here"
+            })
+            alertview.addAction(saveButton)
+            alertview.addAction(cancelButton)
+            self.viewController?.presentViewController(alertview, animated: false, completion: nil)
+
         }
         let cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (alert) -> Void in
             println("Cancel Pressed")
         }
         
         alert.addAction(consultation)
-        alert.addAction(history)
-        alert.addAction(blabla)
+        alert.addAction(changeBed)
+        alert.addAction(changeStatus)
         alert.addAction(cancelButton)
         
         viewController?.presentViewController(alert, animated: true, completion: nil)
